b216507a8053eaa3ea601dbddec386a05aef558e
bensegal2034 <bensegal2034@gmail.com>
2023-12-18 20:30:30 -0500

    Fixed steam.py to use relative paths

--- GMod/steam.py
+++ GMod/steam.py
@@ -10,4 +10,4 @@ local_maps = [str(lmap).split("\\")[-1][:-4] for lmap in list(Path("D:\\SteamCMD
 
 os.system(f"start cmd.exe /k srcds.exe -console +maxplayers 7 -allowlocalhttp +gamemode terrortown +map {random.choice(local_maps)} +sv_setsteamaccount 7F69B937699BB050BE2036DE3448EC17 +host_workshop_collection 2895023437")
 os.system("start cmd /c wsl")
-os.system("npm start --prefix D:\\SteamCMD\\GMod\\Muter")
+os.system("npm start --prefix ./Muter")

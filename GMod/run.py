8480cd21efe043bf4cfd58cdface245605eb50fc
bensegal2034 <bensegal2034@gmail.com>
2024-03-14 21:21:27 -0400

    made run.py wait a bit before launching so that the pull actually works

--- GMod/run.py
+++ GMod/run.py
@@ -1,8 +1,8 @@
 from startserver import startserver
-import os, subprocess
-
-startserver()
+import os, subprocess, time
 
 os.system ("git pull")
+time.sleep(5)
+startserver()
 subprocess.run(["start", "cmd", "/K", "npm start --prefix ./FastDL"], shell=True)
 os.system("npm start --prefix ./Muter")

0a0141736868284039eb03e35c74b27ec23c0846
CosmicFlare3 <120431204+CosmicFlare3@users.noreply.github.com>
2024-06-13 03:13:55 -0400

    workshop.lua comments

--- server/garrysmod/lua/autorun/workshop.lua
+++ server/garrysmod/lua/autorun/workshop.lua
@@ -1,32 +1,91 @@
 if SERVER then
-    resource.AddWorkshop("637848943")
-    resource.AddWorkshop("712797820")
-    resource.AddWorkshop("409155327")
-    resource.AddWorkshop("106404821")
-    resource.AddWorkshop("680737032")
-    resource.AddWorkshop("851715300")
-    resource.AddWorkshop("621565420")
-    resource.AddWorkshop("785294711")
-    resource.AddWorkshop("911182038")
+    --Better Equipment Menu
     resource.AddWorkshop("1107420703")
-    resource.AddWorkshop("748819917")
-    resource.AddWorkshop("276959806")
-    resource.AddWorkshop("295891462")
-    resource.AddWorkshop("1461095720")
-    resource.AddWorkshop("606792331")
-    resource.AddWorkshop("842992984")
-    resource.AddWorkshop("671603913")
+
+    --Hit Numbers
     resource.AddWorkshop("110871780")
-    resource.AddWorkshop("913310851")
+
+    --Dark UI Spawnmenu
     resource.AddWorkshop("888392108")
+
+    --Discord Muter
     resource.AddWorkshop("2155238885")
+
+    --Golden Deagle
+    resource.AddWorkshop("637848943")
+
+    --Tripwire Mine
+    resource.AddWorkshop("712797820")
+
+    --Portal Gun
+    resource.AddWorkshop("409155327")
+
+    --Turtlenade
+    resource.AddWorkshop("106404821")
+
+    --Prop Exploder
+    resource.AddWorkshop("680737032")
+
+    --Trigger Finger Chip
+    resource.AddWorkshop("851715300")
+
+    --Door Buster
+    resource.AddWorkshop("621565420")
+
+    --Taser
+    resource.AddWorkshop("785294711")
+
+    --Headcrab Launcher
+    resource.AddWorkshop("911182038")
+
+    --"Weapon Turret - Buffed"
+    resource.AddWorkshop("748819917")
+
+    --Molotov Cocktail
+    resource.AddWorkshop("276959806")
+
+    --Orbital Bass Cannon
+    resource.AddWorkshop("295891462")
+
+    --Redmatter Bomb
+    resource.AddWorkshop("1461095720")
+
+    --Advanced Disguiser
+    resource.AddWorkshop("606792331")
+
+    --Drachenlord Bomb
+    resource.AddWorkshop("842992984")
+
+    --SATM
+    resource.AddWorkshop("671603913")
+    
+    --Beenade
+    resource.AddWorkshop("913310851")
+
+    --TTT Bots 2.0
     resource.AddWorkshop("1256344426")
+
+    --Exile Hunter
     resource.AddWorkshop("2037655123")
+
+    --Genkidama
     resource.AddWorkshop("2956269658")
+
+    --Kamehameha
     resource.AddWorkshop("1599710095")
+
+    --Bazinga
     resource.AddWorkshop("1431089905")
+
+    --Among Us Grenade (Emergency Meeting Worldmodel)
     resource.AddWorkshop("2622509062")
+
+    --Fuze Cluster Charge
     resource.AddWorkshop("3054944580")
+
+    --Improved Automatic Sentry Gun
     resource.AddWorkshop("2887392114")
+
+    --Amongus Summoner
     resource.AddWorkshop("3135461830")
 end

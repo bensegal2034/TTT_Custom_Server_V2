aff5b922605e96342a4a812612cc0ec4e320f833
CosmicFlare3 <120431204+CosmicFlare3@users.noreply.github.com>
2024-06-18 00:55:47 -0400

    sniper summoner fix???? scary commit.

--- server/garrysmod/lua/autorun/workshop.lua
+++ server/garrysmod/lua/autorun/workshop.lua
@@ -1,91 +1,41 @@
 if SERVER then
-    --Better Equipment Menu
     resource.AddWorkshop("1107420703")
-
-    --Hit Numbers
     resource.AddWorkshop("110871780")
-
-    --Dark UI Spawnmenu
     resource.AddWorkshop("888392108")
-
-    --Discord Muter
     resource.AddWorkshop("2155238885")
-
-    --Golden Deagle
     resource.AddWorkshop("637848943")
-
-    --Tripwire Mine
     resource.AddWorkshop("712797820")
-
-    --Portal Gun
     resource.AddWorkshop("409155327")
-
-    --Turtlenade
     resource.AddWorkshop("106404821")
-
-    --Prop Exploder
     resource.AddWorkshop("680737032")
-
-    --Trigger Finger Chip
     resource.AddWorkshop("851715300")
-
-    --Door Buster
     resource.AddWorkshop("621565420")
-
-    --Taser
     resource.AddWorkshop("785294711")
-
-    --Headcrab Launcher
     resource.AddWorkshop("911182038")
-
-    --"Weapon Turret - Buffed"
     resource.AddWorkshop("748819917")
-
-    --Molotov Cocktail
     resource.AddWorkshop("276959806")
-
-    --Orbital Bass Cannon
     resource.AddWorkshop("295891462")
-
-    --Redmatter Bomb
     resource.AddWorkshop("1461095720")
-
-    --Advanced Disguiser
     resource.AddWorkshop("606792331")
-
-    --Drachenlord Bomb
     resource.AddWorkshop("842992984")
-
-    --SATM
     resource.AddWorkshop("671603913")
-    
-    --Beenade
     resource.AddWorkshop("913310851")
-
-    --TTT Bots 2.0
     resource.AddWorkshop("1256344426")
-
-    --Exile Hunter
     resource.AddWorkshop("2037655123")
-
-    --Genkidama
     resource.AddWorkshop("2956269658")
-
-    --Kamehameha
     resource.AddWorkshop("1599710095")
-
-    --Bazinga
     resource.AddWorkshop("1431089905")
-
-    --Among Us Grenade (Emergency Meeting Worldmodel)
     resource.AddWorkshop("2622509062")
-
-    --Fuze Cluster Charge
     resource.AddWorkshop("3054944580")
-
-    --Improved Automatic Sentry Gun
     resource.AddWorkshop("2887392114")
-
-    --Amongus Summoner
     resource.AddWorkshop("3135461830")
+    resource.AddWorkshop("2892783240")
+	resource.AddWorkshop("2819333362")
+	resource.AddWorkshop("1615324913")
+    resource.AddWorkshop("2887757921")
+    resource.AddWorkshop("2590612326")
+    resource.AddWorkshop("2887392114")
+    resource.AddWorkshop("954453521")
+    resource.AddWorkshop("637848943")
+    resource.AddWorkshop("3066505251")
 end

bdf74338f36f9a2db0517f564c6f68b78cbb183a
bensegal2034 <bensegal2034@gmail.com>
2024-07-24 00:35:40 -0400

    lefthand addon fixed

--- server/garrysmod/addons/left-hand/lua/autorun/client/init_lefthand_cl.lua
+++ server/garrysmod/addons/left-hand/lua/autorun/client/init_lefthand_cl.lua
@@ -22,11 +22,18 @@ weaponDefaults = weaponDefaults or {}
 firstLoad = firstLoad or true
 if not(table.IsEmpty(weaponDefaults)) then buildWeaponDefaultTable() end
 
-local function handleWeapon(wep, currentCvarState)
-    local wepClassName = wep:GetClass()
+local function handleWeapon(wepName, currentCvarState)
+    local wep = LocalPlayer():GetWeapon(wepName)
+    print("blargh")
+    if not(IsValid(wep)) then
+        timer.Simple(0.5, function()
+            handleWeapon(wepName, currentCvarState)
+        end)
+        return
+    end
     local doLeftHand = not(currentCvarState)
-    local default = weaponDefaults[wepClassName]
-    --print("Runnning for " .. wepClassName .. ", default " .. tostring(default))
+    local default = weaponDefaults[wepName]
+    --print("Runnning for " .. wepName .. ", default " .. tostring(default))
     if doLeftHand then
         wep.ViewModelFlip = not(default)
     else
@@ -38,27 +45,16 @@ end
 cvars.AddChangeCallback("cl_righthand", function(cvarName, oldValue, newValue)
     weps = LocalPlayer():GetWeapons()
     for _, wep in ipairs(weps) do
-        handleWeapon(wep, tobool(newValue))
+        handleWeapon(wep:GetClass(), tobool(newValue))
     end
 end)
 
 gameevent.Listen("player_activate")
 hook.Add("player_activate", "InitialLoad", function(data)
-    timer.Simple(5, function()
-        firstLoad = false
-    end)
     buildWeaponDefaultTable()
 end)
 
 net.Receive("NotifyWeaponEquip", function()
     local wepName = net.ReadString()
-    -- this timer needs to be here, else
-    -- the client reads the weapon as null for some reason
-    -- i hate gmod
-    if firstLoad then timing = 3 else timing = 0.1 end
-
-    timer.Simple(timing, function()
-        local wep = LocalPlayer():GetWeapon(wepName)
-        handleWeapon(wep, cvar:GetBool())
-    end)
+    handleWeapon(wepName, cvar:GetBool())
 end)

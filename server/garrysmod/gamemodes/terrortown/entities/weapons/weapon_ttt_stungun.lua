c9f5601e6499156fefed4a25646ab515c7186a0c
CosmicFlare3 <120431204+CosmicFlare3@users.noreply.github.com>
2026-01-07 19:03:40 -0500

    UMP floorgun

--- server/garrysmod/gamemodes/terrortown/entities/weapons/weapon_ttt_stungun.lua
+++ server/garrysmod/gamemodes/terrortown/entities/weapons/weapon_ttt_stungun.lua
@@ -3,8 +3,8 @@ AddCSLuaFile()
 SWEP.HoldType              = "ar2"
 
 if CLIENT then
-   SWEP.PrintName          = "stungun_name"
-   SWEP.Slot               = 6
+   SWEP.PrintName          = "UMP V1.0"
+   SWEP.Slot               = 2
 
    SWEP.ViewModelFlip      = false
    SWEP.ViewModelFOV       = 54
@@ -20,22 +20,25 @@ end
 
 SWEP.Base                  = "weapon_tttbase"
 
-SWEP.Kind                  = WEAPON_STUNGUN
+SWEP.Kind                  = WEAPON_HEAVY
 SWEP.WeaponID              = AMMO_STUN
-SWEP.CanBuy                = {ROLE_DETECTIVE}
+SWEP.CanBuy                = {}
 SWEP.LimitedStock          = false
-SWEP.AmmoEnt               = "item_ammo_smg1_ttt"
+SWEP.AmmoEnt = "item_ammo_smg1_ttt"
 SWEP.Tracer = "AR2Tracer"
-SWEP.Primary.Damage        = 16
-SWEP.Primary.Delay         = 0.1
-SWEP.Primary.Cone          = 0.02
+SWEP.Primary.Damage        = 15
+SWEP.Primary.Delay         = 0.11
+SWEP.Primary.Cone          = 0.04
 SWEP.Primary.ClipSize      = 30
-SWEP.Primary.ClipMax       = 60
-SWEP.Primary.DefaultClip   = 90
+SWEP.Primary.ClipMax       = 120
+SWEP.Primary.DefaultClip   = 60
 SWEP.Primary.Automatic     = true
 SWEP.Primary.Ammo          = "AlyxGun"
-SWEP.Primary.Recoil        = 1.2
+SWEP.Primary.Recoil        = 2.2
 SWEP.Primary.Sound         = Sound( "Weapon_UMP45.Single" )
+
+SWEP.AutoSpawnable = true
+SWEP.DamageType = "Impact"
 
 SWEP.UseHands              = true
 SWEP.ViewModel             = "models/weapons/cstrike/c_smg_ump45.mdl"
@@ -44,8 +47,16 @@ SWEP.WorldModel            = "models/weapons/w_smg_ump45.mdl"
 SWEP.IronSightsPos         = Vector(-8.735, -10, 4.039)
 SWEP.IronSightsAng         = Vector(-1.201, -0.201, -2)
 
-SWEP.HeadshotMultiplier    = 3 -- brain fizz
+SWEP.HeadshotMultiplier    = 1.7 -- brain fizz
 --SWEP.DeploySpeed = 3
+
+SWEP.IsCharged = false
+SWEP.MaxCharge = 180
+SWEP.ChargeTime = 180
+
+function SWEP:SetupDataTables()
+   self:NetworkVar("Int", 1, "ChargeTime")
+end
 
 function SWEP:Shake()
 	if SERVER then
@@ -81,6 +92,9 @@ function SWEP:ShootBullet( dmg, recoil, numbul, cone )
    bullet.TracerName = self.Tracer
    bullet.Force  = 5
    bullet.Damage = dmg
+   if self.ChargeTime >= self.MaxCharge then
+      bullet.Damage = 20
+   end
 
    bullet.Callback = function(att, tr, dmginfo)
       if SERVER or (CLIENT and IsFirstTimePredicted()) then
@@ -91,22 +105,29 @@ function SWEP:ShootBullet( dmg, recoil, numbul, cone )
             edata:SetEntity(ent)
             edata:SetMagnitude(3)
             edata:SetScale(2)
+            if ent:IsPlayer() then
+               if self.ChargeTime >= self.MaxCharge then
+                  util.Effect("TeslaHitBoxes", edata)
+                  self:Shake()
+                  if SERVER then
+                     local entang = ent:EyeAngles()
 
-            util.Effect("TeslaHitBoxes", edata)
-            self:Shake()
-            if SERVER and ent:IsPlayer() then
-               local eyeang = ent:EyeAngles()
+                     local j = 4
+                     entang.pitch = math.Clamp(entang.pitch + math.Rand(-j, j), -90, 90)
+                     entang.yaw = math.Clamp(entang.yaw + math.Rand(-j, j), -90, 90)
+                     ent:SetEyeAngles(entang)
 
-               local j = 10
-               eyeang.pitch = math.Clamp(eyeang.pitch + math.Rand(-j, j), -90, 90)
-               eyeang.yaw = math.Clamp(eyeang.yaw + math.Rand(-j, j), -90, 90)
-               ent:SetEyeAngles(eyeang)
-               local eyeang = ent:EyeAngles()
+                     self.IsCharged = false
+                     self.ChargeTime = 0
+                     self:SetChargeTime(self.ChargeTime)
 
-               local j = 10
-               eyeang.pitch = math.Clamp(eyeang.pitch + math.Rand(-j, j), -90, 90)
-               eyeang.yaw = math.Clamp(eyeang.yaw + math.Rand(-j, j), -90, 90)
-               self.Owner:SetEyeAngles(eyeang)
+                     local eyeang = self:GetOwner():EyeAngles()
+                     eyeang.pitch = eyeang.pitch - recoil * 6
+                     self:GetOwner():SetEyeAngles( eyeang )
+                  end
+                  self:EmitSound( "Weapon_StunStick.Melee_Hit", 75, 100, 1, CHAN_AUTO )
+                  self.ChargeTime = self:GetChargeTime()
+               end
             end
          end
       end
@@ -135,4 +156,54 @@ function SWEP:ShootBullet( dmg, recoil, numbul, cone )
       self:GetOwner():SetEyeAngles( eyeang )
 
    end
+end
+
+function SWEP:Think()
+   self:CalcViewModel()
+
+   if self.ChargeTime < self.MaxCharge then
+		if SERVER then
+         self.ChargeTime = self.ChargeTime + 1
+         self:SetChargeTime(self.ChargeTime)
+		end
+	end
+   self.ChargeTime = self:GetChargeTime()
+end
+
+if CLIENT then
+   function SWEP:DrawHUD()
+      if CLIENT then
+         local barLength = 40
+         local yOffset = 35
+         local yOffsetText = 3
+         local shadowOffset = 2
+         local chargeTime = self.ChargeTime
+         local maxCharge  = self.MaxCharge
+         local x = math.floor(ScrW() / 2) + 63
+         local y = math.floor(ScrH() / 2) - (barLength / 2)
+         local chargePercentage = (chargeTime/maxCharge) * barLength
+         local chargeTimeDelta = math.Clamp(math.Truncate(chargeTime, 1), 0, maxCharge)
+         draw.RoundedBox(0, x, y, 5, barLength, Color(20, 20, 20, 225))
+         draw.RoundedBox(0, x, y, 5, chargePercentage,  Color(255, 255, 0, 255))
+      end
+      return self.BaseClass.DrawHUD(self)
+   end
+end
+
+function SWEP:Initialize()
+   if CLIENT and self:Clip1() == -1 then
+      self:SetClip1(self.Primary.DefaultClip)
+   elseif SERVER then
+      self.fingerprints = {}
+
+      self:SetIronsights(false)
+   end
+
+   self:SetDeploySpeed(self.DeploySpeed)
+
+   -- compat for gmod update
+   if self.SetHoldType then
+      self:SetHoldType(self.HoldType or "pistol")
+   end
+   self:SetChargeTime(120)
 end

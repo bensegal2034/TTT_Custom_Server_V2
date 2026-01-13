ac6faceddda88bdfc2c5f6cf57078248320a20c4
CosmicFlare3 <120431204+CosmicFlare3@users.noreply.github.com>
2026-01-12 23:14:32 -0500

    cube nerf ender pearl removed :(

--- server/garrysmod/addons/tungsten-cube/lua/entities/tungsten_cube.lua
+++ server/garrysmod/addons/tungsten-cube/lua/entities/tungsten_cube.lua
@@ -10,7 +10,7 @@ function ENT:Initialize()
     self:SetMoveType(MOVETYPE_VPHYSICS)
     self:SetSolid(SOLID_VPHYSICS)
     self:SetMaterial("materials/models/linnaeus/tungsten/tungstencube.vmt")
-
+    self.SpawnTime = CurTime()
     local phys = self:GetPhysicsObject()
     if phys:IsValid() then
         phys:Wake()
@@ -22,11 +22,18 @@ function ENT:PhysicsCollide(data, collider)
     if data.DeltaTime < 0.15 then return end
 
     local speed = data.Speed
-    if data.Speed > 250 then
+    
+    if CurTime() - self.SpawnTime > .3 then
+        speed = speed/4
+    else
+        speed = speed/20
+    end
+    if speed > 250 then
+        print(speed)
         if SERVER then
             local ent = data.HitEntity
             if IsValid(ent) and ent != self:GetOwner() then
-                ent:TakeDamage(speed/4, self:GetCreator(), self)
+                ent:TakeDamage(speed, self:GetCreator(), self)
             end
         end
         self:EmitSound("phx/hmetal" .. math.random(1, 3) .. ".wav")

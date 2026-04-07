dae8efd48af95a66c54d57b7f3e93ade7d656139
Ben Segal <bensegal2034@gmail.com>
2026-04-06 22:13:05 -0400

    hopefully fix weird crashes related to decals

--- server/garrysmod/gamemodes/terrortown/gamemode/cl_init.lua
+++ server/garrysmod/gamemodes/terrortown/gamemode/cl_init.lua
@@ -260,7 +260,8 @@ function GM:CleanUpMap()
    end
 
    -- This cleans up decals since GMod v100
-   local removed = util.RemoveDecalsAt(Vector(0, 0, 0), math.huge, 0, false)
+   game.CleanUpMap()
+   --local removed = util.RemoveDecalsAt(Vector(0, 0, 0), math.huge, 0, false)
 end
 
 -- server tells us to call this when our LocalPlayer has spawned

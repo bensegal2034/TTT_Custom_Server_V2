a73be708e790060591b2511cfa9e8523b75e80c0
CosmicFlare3 <120431204+CosmicFlare3@users.noreply.github.com>
2026-05-25 18:03:52 -0400

    xm rework

--- server/garrysmod/gamemodes/terrortown/entities/weapons/weapon_zm_shotgun.lua
+++ server/garrysmod/gamemodes/terrortown/entities/weapons/weapon_zm_shotgun.lua
@@ -22,16 +22,24 @@ SWEP.WeaponID              = AMMO_SHOTGUN
 
 SWEP.Primary.Ammo          = "Buckshot"
 SWEP.Primary.Damage        = 7
-SWEP.Primary.Cone          = 0.15
+SWEP.Primary.Cone          = 0.17
 SWEP.Primary.Delay         = 0.45
 SWEP.Primary.ClipSize      = 8
 SWEP.Primary.ClipMax       = 24
 SWEP.Primary.DefaultClip   = 16
 SWEP.Primary.Automatic     = true
-SWEP.Primary.NumShots      = 10
+SWEP.Primary.NumShots      = 8
 SWEP.Primary.Sound         = Sound( "Weapon_XM1014.Single" )
 SWEP.Primary.Recoil        = 12
-SWEP.SavedPrimaryCone = 0.15
+SWEP.SavedPrimaryCone = 0.17
+
+--Absolute minimum value the cone can reach
+SWEP.MinCone = 0.01
+--SWEP.Charging is used to tell other methods that we are currently holding the button down, attempting to charge a shot, mainly telling the primaryattack method to return until the key is released
+SWEP.Charging = false
+
+--SWEP.Fired is used to make sure we dont repeatedly attempt to fire the shot well after the key has been released and the shot is fired
+SWEP.Fired = false
 
 SWEP.Secondary.Damage   = 40
 SWEP.Secondary.Sound    = Sound("weapons/slug.wav")
@@ -43,6 +51,8 @@ SWEP.Secondary.Recoil   = 8
 SWEP.DamageType            = "Impact"
 
 SWEP.Secondary.Automatic   = true
+
+SWEP.HeadshotMultiplier    = 1.5
 
 SWEP.AutoSpawnable         = true
 SWEP.Spawnable             = true
@@ -58,7 +68,7 @@ SWEP.IronSightsAng         = Vector(-0.101, -0.7, -0.201)
 function SWEP:SetupDataTables()
    self:NetworkVar("Bool", 0, "Reloading")
    self:NetworkVar("Float", 0, "ReloadTimer")
-
+   self:NetworkVar("Float", 0, "ChargeValue")
    return BaseClass.SetupDataTables(self)
 end
 
@@ -132,29 +142,26 @@ function SWEP:FinishReload()
 end
 
 function SWEP:CanPrimaryAttack()
-   if self:Clip1() <= 0 then
-      self:EmitSound( "Weapon_Shotgun.Empty" )
-      self:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
-      self:SetNextSecondaryFire( CurTime() + self.Secondary.Delay )
-      return false
+   if self:Clip1() <= 0 and self:GetNextPrimaryFire() <= CurTime() then
+      if self:GetOwner():KeyDown(IN_ATTACK) then
+         self:EmitSound( "Weapon_Shotgun.Empty" )
+         self:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
+         self:SetNextSecondaryFire( CurTime() + self.Secondary.Delay )
+         return false
+      end
    end
    return true
 end
 
-function SWEP:CanSecondaryAttack()
-   if self:Clip1() <= 0 then
-      self:EmitSound( "Weapon_Shotgun.Empty" )
-      self:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
-      self:SetNextSecondaryFire( CurTime() + self.Secondary.Delay )
-      return false
-   end
-   return true
+function SWEP:Initialize()
+   self.BaseClass.Initialize(self)
+   self:SetChargeValue(0.17)
 end
 
 function SWEP:Think()
    self.BaseClass.Think(self)
    if self:GetReloading() then
-      if self:GetOwner():KeyDown(IN_ATTACK) or self:GetOwner():KeyDown(IN_ATTACK2) then
+      if self:GetOwner():KeyDown(IN_ATTACK) then
          self:FinishReload()
          return
       end
@@ -170,6 +177,46 @@ function SWEP:Think()
          end
          return
       end
+   else
+      if self:CanPrimaryAttack() and self:GetNextPrimaryFire() <= CurTime() and self:GetReloading() == false then
+         if self:GetOwner():KeyDown(IN_ATTACK) then
+            self.Charging = true
+            self.Fired = false
+            if SERVER then
+               if self.Primary.Cone > self.MinCone then
+                  self:SetChargeValue(self.Primary.Cone - 0.00175)
+                  self.Primary.Cone = self:GetChargeValue()
+                  if self.Primary.Cone < self.MinCone then
+                     self.Primary.Cone = self.MinCone
+                     self:SetChargeValue(self.MinCone)
+                     self:EmitSound("Grenade.Blip")
+                  end
+               end
+            end
+            --This is done exclusively to tell the client to reduce the size of the crosshair, giving a visual indicator of how charged the shot is
+            if CLIENT then
+               self.Primary.Cone = self:GetChargeValue()
+            end
+            --adds very light amount of screen shake while charging (specifically after button has been held for a while, to prevent spam click having insane screen shake)
+            --i would like to make only the viewmodel shake but this is a temporary solution for vfx
+            if self.Primary.Cone < 0.1 then
+               self:StartLoopingSound("Weapon_MegaPhysCannon.HoldSound")
+               self.Soundid = self:StartLoopingSound("Weapon_MegaPhysCannon.HoldSound")
+               util.ScreenShake( Vector(0, 0, 0), 0.015, 0.015, 0.015, 5000 )
+            end
+         elseif self:GetOwner():KeyReleased(IN_ATTACK) and self.Fired == false then
+            self:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
+            self.Charging = false
+            self:PrimaryAttack()
+
+            --Post firing reset
+            self.CurrentCharge = 0
+            if SERVER then
+               self:SetChargeValue(0.17)
+            end
+            self.Primary.Cone = self.SavedPrimaryCone
+         end
+      end
    end
 end
 
@@ -179,66 +226,67 @@ function SWEP:Deploy()
    return BaseClass.Deploy(self)
 end
 
--- The shotgun's headshot damage multiplier is based on distance. The closer it
--- is, the more damage it does. This reinforces the shotgun's role as short
--- range weapon by reducing effectiveness at mid-range, where one could score
--- lucky headshots relatively easily due to the spread.
-function SWEP:GetHeadshotMultiplier(victim, dmginfo)
-   local att = dmginfo:GetAttacker()
-   if not IsValid(att) then return 3 end
-
-   local dist = victim:GetPos():Distance(att:GetPos())
-   local d = math.max(0, dist - 140)
-
-   -- Decay from 2 to 1 slowly as distance increases. Note that this used to be
-   -- 3+, but at that time shotgun bullets were treated like in HL2 where half
-   -- of them were hull traces that could not headshot.
-   return 1 + math.max(0, (2.1 - 0.002 * (d ^ 1.25)))
-end
-
-function SWEP:SecondaryAttack()
-	self:SetNextSecondaryFire( CurTime() + self.Secondary.Delay )
-	self:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
-
-	if not self:CanSecondaryAttack() then return end
-
-	if not worldsnd then
-		self:EmitSound( self.Primary.Sound, self.Primary.SoundLevel )
-	elseif SERVER then
-		sound.Play(self.Primary.Sound, self:GetPos(), self.Primary.SoundLevel)
-	end
-	--This is done exclusively to trick the crosshair into displaying the inaccuracy of right clicking, which is reset on primary attack
-	self.Primary.Cone = self.Secondary.Cone
-
-	self:ShootBullet( self.Secondary.Damage, self.Secondary.Recoil, self.Secondary.NumShots, self.Secondary.Cone )
-	
-	self:TakePrimaryAmmo( 1 )
-
-	local owner = self:GetOwner()
-	if not IsValid(owner) or owner:IsNPC() or (not owner.ViewPunch) then return end
-
-	owner:ViewPunch( Angle( util.SharedRandom(self:GetClass(),-0.2,-0.1,0) * self.Secondary.Recoil, util.SharedRandom(self:GetClass(),-0.1,0.1,1) * self.Secondary.Recoil, 0 ) )
-end
-
 function SWEP:PrimaryAttack()
 	self:SetNextSecondaryFire( CurTime() + self.Primary.Delay )
-	self:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
 
 	if not self:CanPrimaryAttack() then return end
+   
+   
+
+   if self.Charging then return end
+   
+   self:StopLoopingSound(self.Soundid)
+
+   if self.Primary.Cone < 0.025 then
+      self.Primary.Sound = Sound("weapons/slug.wav")
+   else
+      self.Primary.Sound = Sound( "Weapon_XM1014.Single" )
+   end
 
 	if not worldsnd then
 		self:EmitSound( self.Primary.Sound, self.Primary.SoundLevel )
 	elseif SERVER then
 		sound.Play(self.Primary.Sound, self:GetPos(), self.Primary.SoundLevel)
 	end
-	-- Resetting primary cone to what it should be before shooting
-	self.Primary.Cone = self.SavedPrimaryCone
+   --the viewmodel will mysteriously not play the attack animation when firing. dont know why, band-aid attempt to fix that did not work
+   self:SendWeaponAnim( ACT_VM_PRIMARYATTACK )
 
 	self:ShootBullet( self.Primary.Damage, self.Primary.Recoil, self.Primary.NumShots, self:GetPrimaryCone() )
 	self:TakePrimaryAmmo( 1 )
-
+   
+   --Delay is added here in a fallback situation where the attack is called twice in rapid succession, ex: a bug allowing the player to instantly fire two shots if attempting to cancel a reload with firing
+   self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
 	local owner = self:GetOwner()
 	if not IsValid(owner) or owner:IsNPC() or (not owner.ViewPunch) then return end
+   self.Fired = true
+   self.Primary.Cone = self.SavedPrimaryCone
 
 	owner:ViewPunch( Angle( util.SharedRandom(self:GetClass(),-0.2,-0.1,0) * self.Primary.Recoil, util.SharedRandom(self:GetClass(),-0.1,0.1,1) * self.Primary.Recoil, 0 ) )
+end
+
+function SWEP:SecondaryAttack()
+
+end
+
+function SWEP:Holster()
+   self:StopLoopingSound(self.Soundid)
+   self.Charging = false
+   self.CurrentCharge = 0
+   if SERVER then
+      self:SetChargeValue(0.17)
+   end
+   self.Primary.Cone = self.SavedPrimaryCone
+   return true
+end
+
+function SWEP:PreDrop()
+   self:StopLoopingSound(self.Soundid)
+   self:StopLoopingSound(self.Soundid)
+   self.Charging = false
+   self.CurrentCharge = 0
+   if SERVER then
+      self:SetChargeValue(0.17)
+   end
+   self.Primary.Cone = self.SavedPrimaryCone
+   self.BaseClass.PreDrop(self)
 end
